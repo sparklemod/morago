cdf4bd38da1e722bc15f1db1e7627c3bf3e4c385
rina <katerina5314@mail.ru>
2025-05-09 02:18:59 +0300

    init project add .env, application.yml, pom.xml, docker-compose.yaml, README.md

---
+++ src/main/java/morago_backend/MoragoBackendApplication.java
@@ -0,0 +1,13 @@
+package morago_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoragoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoragoBackendApplication.class, args);
+	}
+
+}
