af7b6f04116a90f9a21e38cc032ea41f6bc08641
xessar <a.listep@yandex.ru>
2025-07-29 19:15:26 +0900

    fix

--- src/main/java/com/example/morago/config/OpenApi/OpenApiConfig.java
+++ src/main/java/com/example/morago/config/OpenApi/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.example.morago.config.openApi;
+package com.example.morago.config.OpenApi;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
--- src/main/java/com/example/morago/config/security/userDetails/CustomUserDetailsService.java
+++ src/main/java/com/example/morago/config/security/userDetails/CustomUserDetailsService.java
@@ -3,13 +3,12 @@ package com.example.morago.config.security.userDetails;
 import com.example.morago.model.entity.base.User;
 import com.example.morago.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserService userService;
 
